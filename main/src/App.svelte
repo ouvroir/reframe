b99e8bd4fc37051f014f0f8498ae677bf6715058
william.diakite@gmail.com <william.diakite@gmail.com>
2023-11-06 16:39:09 -0500

    setup svelte - first prototype

---
+++ main/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Gallery from "./lib/Gallery.svelte";
+</script>
+
+<main>
+  <h1>Reframe prototype november 2023</h1>
+
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ea omnis
+    explicabo accusamus quo facilis laboriosam, quos corporis molestiae nostrum
+    ad optio. Earum veniam omnis consectetur fugit doloribus soluta nobis.
+  </p>
+
+  <Gallery />
+</main>
+
+<style>
+</style>
